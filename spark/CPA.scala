bdd3d6312a9923e0e6e0712f7824e38b7e3b56c2
Inaba <mas178@users.noreply.github.com>
2016-05-09 14:39:39 +0900

    Create CPA.scala

---
+++ spark/CPA.scala
@@ -0,0 +1,35 @@
+package hellospark
+
+import breeze.stats.distributions.Gamma
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
+
+object CPA {
+
+  def main(args: Array[String]) {
+
+    val conf = new SparkConf().setMaster("local[*]").setAppName("CPAのサンプル")
+    val sc = new SparkContext(conf)
+
+    val c = 20
+    val gamma = new Gamma(100, 1.0 / 100)
+
+    val vectors = (0 until c)
+      .map(x => gamma.sample(c))
+      .map(x => Vectors.dense(x.toArray))
+
+    val rowRdd = sc.parallelize(vectors)
+    val rowMatrix = new RowMatrix(rowRdd)
+    val pcMatrix = rowMatrix.computePrincipalComponents(5)
+
+    val toString =
+      (0 until pcMatrix.numRows).map { rowNum =>
+        (0 until pcMatrix.numCols).map { colNum =>
+          pcMatrix(rowNum, colNum)
+        }.mkString("\t")
+      }.mkString("\n")
+
+    println(toString)
+  }
+}
